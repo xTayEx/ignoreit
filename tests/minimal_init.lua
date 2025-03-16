cf19a0806c3ba0833880d1b1113f8917ab06b861
xtayex <liyuliang2022_sei@stu.ecnu.edu.cn>
2025-03-16 23:51:06 +0800

    fix: modify minimal_init.lua for import telescope and nui

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,11 +1,21 @@
-local plenary_dir = os.getenv("PLENARY_DIR") or "/tmp/plenary.nvim"
-local is_not_a_directory = vim.fn.isdirectory(plenary_dir) == 0
-if is_not_a_directory then
-  vim.fn.system({"git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir})
+local setup_plugin = function(plugins)
+  for _, plugin in ipairs(plugins) do
+    local plugin_dir = "/tmp/" .. vim.split(plugin, "/")[2]
+    local is_not_a_directory = vim.fn.isdirectory(plugin_dir) == 0
+    if is_not_a_directory then
+      vim.fn.system({ "git", "clone", "https://github.com/" .. plugin, plugin_dir })
+    end
+    vim.opt.rtp:append(plugin_dir)
+  end
 end
 
+setup_plugin({
+  "nvim-lua/plenary.nvim",
+  "MunifTanjim/nui.nvim",
+  "nvim-telescope/telescope.nvim"
+})
+
 vim.opt.rtp:append(".")
-vim.opt.rtp:append(plenary_dir)
 
 vim.cmd("runtime plugin/plenary.vim")
 require("plenary.busted")
